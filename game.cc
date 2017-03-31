b2f4b2fce1ed5d84d0c6c8239d215d7d7a6c7428
Mike <michael.argany@gmail.com>
2017-03-30 22:32:41 -0400

    Fuckboy Asian mother fucker ass clapper

--- game.cc
+++ game.cc
@@ -1,9 +1,20 @@
 #include "game.h"
+#include <string> 
 
 Game::Game() { g = nullptr; }
 
 void Game::startGame(std::ifstream &ifs) {
 	g = new Grid(ifs);
-}
+	string input;
+	
 
-bool Game::running() { }
+
+	while (g.player->getHealth() > 0 && cin >> input) {
+		g.displayGrid();
+		std::cout << "Race: " << g.player->getRace() << " Gold: " << g.player->getGold() << std::endl;
+		std::cout << "HP: " << g.player->getHealth() << std::endl;
+		std::cout << "Atk: " << g.player->getAtk() << std::endl;
+		std::cout << "Def: " << g.player->getDef() << std::endl;
+		std::cout << "Action: " << std::endl;
+	}
+}
